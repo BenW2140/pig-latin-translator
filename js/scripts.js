c97ae321b3941773bb8fc9fe66952d3f05a3129f
Ben White <bwhite2140@outlook.com>
2020-06-10 12:38:27 -0700

    add business logic for first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
+const translator = function (language, vowels) {
+  return language;
+}
 
+$(document).ready(function() {
+  $("form#translator").submit(function(event) {
+    event.preventDefault();
+
+    const english = $("input#english").val();
+    const vowels = ["a", "e", "i", "o", "u"];
+    const pigLatin = translator(english, vowels);
+    $("#pig-latin").text(english);
+  });
 });
